22bed9f6543999b7b3ad638c1e3c8550e3eceb9f
mengziQ <moekoasano0829@gmail.com>
2018-03-28 01:13:33 +0900

    jQuery練習

--- pv-counter/pv_article.py
+++ pv-counter/pv_article.py
@@ -1,9 +1,6 @@
 # coding: shift_jis
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
 from time import sleep
 
 options = Options()
