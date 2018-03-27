1c5287dc94a0a14400f09e218babc1a49517b8c6
mengziQ <moekoasano0829@gmail.com>
2018-03-28 01:00:21 +0900

    何とか完成 jQuery様様です

--- pv-counter/pv_article.py
+++ pv-counter/pv_article.py
@@ -9,14 +9,12 @@ from time import sleep
 options = Options()
 options.binary_location = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
 
-while True:
+for i in range(10):
   driver = webdriver.Chrome(chrome_options=options, executable_path="C:\\chromedriver\\chromedriver.exe")
   driver.get("https://withonline.jp/")
   driver.find_elements_by_css_selector("html > body > div.header > div.container > div.search-icon > a > img")[0].click()
   driver.find_elements_by_css_selector("html > body > div.header > div.container > div.search-icon > form > div > input")[0].send_keys("ヘイローアイ")
-  WebDriverWait(driver, 5).until(ec.invisibility_of_element_located((By.CSS_SELECTOR, "html > body > div.header > div.container > div.search-icon > form > div > button > img")))
-  driver.find_elements_by_css_selector("html > body > div.header > div.container > div.search-icon > form > div > button > img")[0].click()
-  driver.find_elements_by_css_selector("html > body > div.content > div.row > div.col-sm-12 > div.content > div.row > div.col-sm-8 > div.list-group > div.article-4zlJ4 > a").click()
-  sleep(10)
+  driver.execute_script("$('html > body > div.header > div.container > div.search-icon > form > div > button').click()")
+  driver.find_elements_by_css_selector("html > body > div.content > div.row > div.col-sm-12 > div.content > div.row > div.col-sm-8 > div.list-group > div.article-4zlJ4 > a")[0].click()
   driver.close()
-  sleep(1)
+  sleep(10)
